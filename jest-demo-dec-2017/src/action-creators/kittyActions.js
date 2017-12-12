4ae562c0fae50fd82535269f2c9c1317cc446a55
David Mendez <davidm@boldcommerce.com>
2017-12-12 16:24:55 -0600

    action creator, reducers, and normal components

--- jest-demo-dec-2017/src/action-creators/kittyActions.js
+++ jest-demo-dec-2017/src/action-creators/kittyActions.js
@@ -1,6 +1,9 @@
-export function addKitty() {
+import 'whatwg-fetch'
+
+export function addKitty(name) {
     return {
-        type: "ADD_A_KITTY"
+        type: "ADD_A_KITTY",
+        name,
     }
 }
 
@@ -9,3 +12,34 @@ export function removeKitty() {
         type: "REMOVE_A_KITTY"
     }
 }
+
+function fetchIPRequest() {
+  return {
+    type: "FETCH_IP_REQUEST"
+  }
+}
+
+function fetchIPSuccess(body) {
+  return {
+    type: "FETCH_IP_SUCCESS",
+    body
+  }
+}
+
+function fetchIPFailure(ex) {
+  return {
+    type: "FETCH_IP_FAILURE",
+    ex
+  }
+}
+
+export function fetchIP() {
+  return dispatch => {
+    dispatch(fetchIPRequest())
+    return fetch('http://ip.jsontest.com/')
+      .then(res => res.json())
+      .then(body => dispatch(fetchIPSuccess(body)))
+      .catch(ex => dispatch(fetchIPFailure(ex)))
+  }
+}
+
--- jest-demo-dec-2017/src/test/App.redux.test.js
+++ jest-demo-dec-2017/src/test/App.redux.test.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock'
 import App from '../App';
 import { Provider } from 'react-redux';
 import configureStore from '../store/configureStore';
 import { AddKittyButton } from '../components/Kitty';
-import { addKitty, removeKitty } from '../action-creators/kittyActions';
+import { addKitty, removeKitty, fetchIP } from '../action-creators/kittyActions';
 import kittyReducer from '../reducers/kittyReducer';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 
 const store = configureStore();
@@ -32,6 +37,7 @@ it('renders without crashing react-test-renderer', () => {
     );
 });
 
+//Component rendering
 it('Add Kitty Button Renders with proper properties.', () => {
     const kittenButton = renderer.create(<AddKittyButton onClickCallback={e => 1} />);
     let tree = kittenButton.toJSON();
@@ -39,17 +45,55 @@ it('Add Kitty Button Renders with proper properties.', () => {
     expect(tree).toMatchSnapshot();
 });
 
+//AddKitty Action Creators
 it('Add Kitty and Remove Kitty Action creators', () => {
-    expect(addKitty()).toEqual({type: 'ADD_A_KITTY'});
-    expect(removeKitty()).toEqual({type: 'REMOVE_A_KITTY'});
+    expect(addKitty()).toEqual({ type: 'ADD_A_KITTY' });
+    expect(removeKitty()).toEqual({ type: 'REMOVE_A_KITTY' });
 });
 
-it('kitty reducer test', () => {
-    let initialState = {kitties: 0};
-    expect(kittyReducer(initialState, {type:'ADD_A_KITTY'}).kitties).toEqual(1);
-    expect(kittyReducer(initialState, {type:'REMOVE_A_KITTY'}).kitties).toEqual(0);
-    initialState.kitties = 2;
-    expect(kittyReducer(initialState, {type:'REMOVE_A_KITTY'}).kitties).toEqual(1);
+//Kitty Reducers
+describe('kitty reducers', () => {
+    let initialState = { kitties: 0 };
+    
+    it('Add a kitty', () => {
+        expect(kittyReducer(initialState, { type: 'ADD_A_KITTY' }).kitties).toEqual(1);
+    });
+    it('Do not go below 0 when removing a kitty', () => {
+        expect(kittyReducer(initialState, { type: 'REMOVE_A_KITTY' }).kitties).toEqual(0);
+    });
+    it('Remove a kitty', () => {
+        initialState.kitties = 2;
+        expect(kittyReducer(initialState, { type: 'REMOVE_A_KITTY' }).kitties).toEqual(1);
+    });
 })
+
+
+///get our I.P address
+describe('async action creators', () => {
+    afterEach(() => {
+        fetchMock.reset()
+        fetchMock.restore()
+    });
+
+    it('Creates FETCH_IP_SUCCESS after fetching IP - Thunk style', () => {
+        fetchMock.getOnce('http://ip.jsontest.com/', { body: { ip: '127.0.99.99' } });
+
+        const expectedActions = [
+            { type: "FETCH_IP_REQUEST" },
+            { type: "FETCH_IP_SUCCESS", body: { ip: '127.0.99.99' } }
+        ];
+
+        const store = mockStore({ ip: '' });
+
+        return store.dispatch(fetchIP()).then(() => {
+            // return of async actions
+            expect(store.getActions()).toEqual(expectedActions)
+          })
+    });
+});
+
+describe('connected component', () => {
+
+});
 
 
